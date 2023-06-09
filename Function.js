31c8302559daf4154e877776e699e4f4b58903cf
Kandy1703 <128167483+Kandy1703@users.noreply.github.com>
2023-06-09 11:56:32 +0530

    frist-commit

---
+++ Function.js
@@ -0,0 +1,30 @@
+// Q1. Create one function with zero parameter having a console statement;
+function name(){
+
+    console.log();
+};
+
+//Q2. Create one function which takes two values as a parameter and print the sum of them as "Sum of 3, 4 is 7"
+
+const a =[3,4,7]
+let sum =0
+
+a.forEach((x)=>{
+    sum=x+sum
+})
+console.log(sum)  //output 14
+
+// Q3.Create one arrow function
+const function() =>
+
+
+// Q4. "Print output
+
+var x = 21;
+
+var girl = function () {
+    console.log(x);
+    var x = 20;
+};
+girl();
+
